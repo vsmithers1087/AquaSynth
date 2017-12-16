afd4b8f02a48fff489e97edf1da6b4f793681e84
vsmithers1087 <vsmithers1087@gmail.com>
2017-12-16 12:42:21 -0500

    add image icons

--- AquaSynth/Model/PredictionService.swift
+++ AquaSynth/Model/PredictionService.swift
@@ -25,15 +25,37 @@ public class AsynthPredictionService: NSObject {
         guard let buffer  = image.toPixelBuffer(image: image, dimension: dimension) else { fatalError("Could not convert image to pixel buffer") }
         
         if let prediction = try? model.prediction(data: buffer) {
+            var currentStill: Double = 0
+            var currentDisturbed: Double = 0
+            var currentXa: Double = 0
+            
             prediction.prob.forEach({ (label, score) in
                 var realNum = score * Double(100)
-                if label == "disturbedA" {
-                    realNum -= 16
+                switch label {
+                case "still":
+                    print("STILL \(realNum)")
+                    currentStill = realNum
+                    //realNum += 9
+                case "disturbedA":
+                    print("disturbed \(realNum)")
+                    currentDisturbed = realNum
+                case "xA":
+                    print("xA \(realNum)")
+                    currentXa = realNum
+                default: break
                 }
-                if realNum > currentScore && realNum  > 0 {
+                
+                if realNum > currentScore && (realNum  > 0  && realNum < 100){
+                    let allGreaterThanZero = currentStill > 0 && currentDisturbed > 0 && currentXa > 0
+                    let onlyXaGreaterThanZero = currentStill < 0 && currentDisturbed < 0 && currentXa > 0
+                    
                     currentScore = realNum
                     currentLabel = label
                     result = AsynthResult(className: label, probability: currentScore)
+                    if allGreaterThanZero {
+                        let minVal = [currentStill, currentDisturbed, currentXa].min()
+                        
+                    }
                 }
             })
             currentScore = 0
