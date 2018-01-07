abea584bcea35b17c20cd2c64f83032df06d876b
vsmithers1087 <vsmithers1087@gmail.com>
2018-01-07 15:01:34 -0500

    refactor resonanceSoundMap

--- AquaSynth/Model/ResonanceSoundMap.swift
+++ AquaSynth/Model/ResonanceSoundMap.swift
@@ -11,20 +11,18 @@ import AudioKit
 
 class ResonanceSoundMap {
     
-    //public var predictionsPerNote: Int
-    private var asynthResults = [AsynthResult]()
-    private var frequencies = [Int]()
-    let bells = AKTubularBells()
-    let string = AKPluckedString()
-    var delay: AKDelay
-    var reverb: AKReverb
+    var currentNoteNumber: UInt8 = 0
+
+    private var delay: AKDelay
+    private var reverb: AKReverb
     private var mixer: AKMixer
-    private var hornWave = WavBase(filename: "horn.wav")
-    private var cricket = WavBase(filename: "cricket.wav")
+    private let bells = AKTubularBells()
+    private let cricket = WavBase(filename: "cricket.wav")
+    private let leftOscillator = AKOscillatorBank(waveform: AKTable(.sine), attackDuration: 0.1, releaseDuration: 0.1)
+    private let rightOscillator = AKOscillatorBank(waveform: AKTable(.sine), attackDuration: 0.1, releaseDuration: 0.1)
     
     init() {
-        hornWave.player.volume = 0.4
-        mixer = AKMixer(bells, string, cricket.player, hornWave.player)
+        mixer = AKMixer(bells, cricket.player, leftOscillator, rightOscillator)
         delay = AKDelay(mixer)
         reverb = AKReverb(delay)
         AudioKit.output = reverb
@@ -35,24 +33,31 @@ class ResonanceSoundMap {
         var triggerFreq: Double = 0
         switch level {
         case .noBowl:
-            triggerFreq = 20
             delay.time = 0
             reverb.dryWetMix = 0
             cricket.player.play()
-            bells.trigger(frequency: triggerFreq.midiNoteToFrequency())
+            leftOscillator.stop(noteNumber: currentNoteNumber)
+            rightOscillator.stop(noteNumber: currentNoteNumber)
+            currentNoteNumber = UInt8(triggerFreq)
         case .still:
             triggerFreq = 43 + prediction / 5
-            delay.time = 2
-            reverb.dryWetMix = 0.3
-            bells.trigger(frequency: triggerFreq.midiNoteToFrequency())
-            string.trigger(frequency: triggerFreq.midiNoteToFrequency())
+            delay.time = 0
+            reverb.dryWetMix = 0
+            leftOscillator.pitchBend = 0
+            rightOscillator.pitchBend = 0
+            leftOscillator.play(noteNumber: 58, velocity: 80)
+            rightOscillator.play(noteNumber: 59, velocity: 80)
+            currentNoteNumber = UInt8(triggerFreq)
         case .disturbed:
             triggerFreq = 70 + prediction / 5
             delay.time = 3
-            reverb.dryWetMix = 0.6
+            reverb.dryWetMix = 0.9
+            leftOscillator.pitchBend = 0.3
+            rightOscillator.pitchBend = 0.5
             bells.trigger(frequency: triggerFreq.midiNoteToFrequency())
-            string.trigger(frequency: (triggerFreq - 20).midiNoteToFrequency())
-            hornWave.player.play()
+            leftOscillator.play(noteNumber: 65, velocity: 80)
+            rightOscillator.play(noteNumber: 66, velocity: 80)
+            currentNoteNumber = UInt8(triggerFreq)
         default:
             break
         }
